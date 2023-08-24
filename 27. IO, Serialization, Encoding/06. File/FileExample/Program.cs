c7bfcb302b57c234fd9f304cb71a6295b7f0fd1f
Hypeomit <136635238+hypeomit@users.noreply.github.com>
2023-08-24 11:01:34 +0545

    File Comments

--- 27. IO, Serialization, Encoding/06. File/FileExample/Program.cs
+++ 27. IO, Serialization, Encoding/06. File/FileExample/Program.cs
@@ -9,19 +9,19 @@ namespace FileExample
         {
             string filePath = @"c:\practice\India.txt", filePath2 = @"c:\practice\India2.txt", filePath3 = @"c:\practice\another.txt";
 
-            File.Create(filePath).Close();
+            File.Create(filePath).Close(); //Create Method
             Console.WriteLine("India.txt created");
 
-            bool exists = File.Exists(filePath);
+            bool exists = File.Exists(filePath);//Exists Method
             if (exists)
             {
-                File.Copy(filePath, filePath2);
+                File.Copy(filePath, filePath2);//Copy Method 
                 Console.WriteLine("Copied India.txt to India2.txt");
 
-                File.Move(filePath2, filePath3);
+                File.Move(filePath2, filePath3);//Move Method
                 Console.WriteLine("Moved India2.txt to another.txt");
 
-                File.Delete(filePath3);
+                File.Delete(filePath3);//Delete Method
                 Console.WriteLine("another.txt deleted");
             }
             else
